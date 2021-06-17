57431b4dd138ab28e63f9600cbfc8b70dfb11e2a
홍지훈 <86041016+hongjihoon1021@users.noreply.github.com>
2021-06-17 16:50:25 +0900

    문자열 복습
    jonahyan helped making this filetanks!

---
+++ dsewde.py
@@ -0,0 +1,12 @@
+name=input('이름을입력해주세요')
+#문자열 포메팅
+print('당신의 이름은 %s이다'%(name))
+#문자열 인덱스
+print('당신의성은 %s이다'%(name[0]))
+#문자열 슬라이싱
+print('당신의이름은 %s이다'%(name[1:3]))
+#문자열개수세기
+print('당신의이름의 글자수는은 %d이다'%len(name))
+#문자열  특정 문자개수 세기(count)
+print('n:%d'%name.count('n'))
+print('a:%d'%name.count('a'))
