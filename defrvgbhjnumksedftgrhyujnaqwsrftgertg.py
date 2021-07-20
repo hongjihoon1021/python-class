c59494e8f8be1583e01f45cc53f5ed0c92895e9a
홍지훈 <86041016+hongjihoon1021@users.noreply.github.com>
2021-07-20 14:45:18 +0900

    ㅇㄴㄹㄷㅊㅊㅍ ㅍㅎㄴㅌㅈㅇ퓨ㅠ

---
+++ defrvgbhjnumksedftgrhyujnaqwsrftgertg.py
@@ -0,0 +1,11 @@
+'''
+age=int(input('당신의나이는 몇살입니까'))
+print("당신의나이는 %d 이군요" %(age))
+'''
+print("두정수를입력하세요")
+num1,num2=input("두수를입력해주세요").split()
+num1=int(num1)
+num2=int(num2)
+print("%d+%d=%d" %(num1, num2, num1+num2))
+print("%d*%d=%d" %(num1, num2, num1*num2))
+
